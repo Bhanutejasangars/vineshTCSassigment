e271028ffa787dcddcc5400579ee7cb7004549f4
BHANUTEJA SANGARS <88140271+Bhanutejasangars@users.noreply.github.com>
2022-11-07 21:32:13 +0530

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+function loginFunction() {
+  console.log("dfghj");
+  window.location.href = "createCustomer.html";
+}
+
+function createFunction() {
+  window.location.href = "updateCustomer.html";
+}
+
+function updateFunction() {
+  window.location.href = "deleteCustomer.html";
+}
+
+function deleteFunction() {
+  window.location.href = "login.html";
+}
